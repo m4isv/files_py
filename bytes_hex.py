c1f629a4ffb45c9c787d8f82e4991f0b07c87d49
M4isv <boladaomailson@gmail.com>
2022-05-15 19:24:25 -0300

    Add files via upload

---
+++ bytes_hex.py
@@ -0,0 +1,18 @@
+
+def sum(a,b):
+	return a+b
+	
+print(sum(10,10))
+
+
+aprendendo hexdecimal
+
+print(bytes.fromhex('68656c6c6f').decode('utf-8'))
+
+
+x = '0x68 0x65 0x6c 0x6c 0x6f'
+
+print(bytes.fromhex('68656c6c6f20776f726c64').decode())
+
+print(b'maria'.hex())
+
